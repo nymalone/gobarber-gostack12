895857025c876d7dceb1159f757d0c79a3419a93
nykolle <malone.nykolle@gmail.com>
2020-07-25 09:10:21 -0300

    file upload route created

--- backend-gobarber/src/routes/users.routes.ts
+++ backend-gobarber/src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
     try {
@@ -23,5 +28,17 @@ usersRouter.post('/', async (req, res) => {
         return res.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (req, res) => {
+        // eslint-disable-next-line no-console
+        console.log(req.file);
+
+        return res.json({ ok: true });
+    },
+);
 
 export default usersRouter;
