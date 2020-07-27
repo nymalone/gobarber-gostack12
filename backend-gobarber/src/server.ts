499730b4563f94ebc293a1e5d11df444d3160948
nykolle <malone.nykolle@gmail.com>
2020-07-27 15:32:25 -0300

    global exception handling added

--- backend-gobarber/src/server.ts
+++ backend-gobarber/src/server.ts
@@ -1,8 +1,12 @@
+/* eslint-disable no-console */
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -12,9 +16,25 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.get('/', (req, res) => res.json({ message: 'Hello rocket!' }));
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+    // vou verificar se o meu erro é uma instacia da classe AppError
+    if (err instanceof AppError) {
+        // se for quer dizer que foi um erro originado pela minha aplicação
+        return res.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    // se for um erro que eu não conheço
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 app.listen(3333, () => {
-    // eslint-disable-next-line no-console
     console.log('🚀  Server started on port 3333!');
 });
