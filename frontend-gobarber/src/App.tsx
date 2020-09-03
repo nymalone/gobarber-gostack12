884581ad59ede0afe99d122899450df39f1d31cb
Nykolle Malone <malone.nykolle@gmail.com>
2020-09-03 17:03:28 -0300

    login styles done

--- frontend-gobarber/src/App.tsx
+++ frontend-gobarber/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import Signin from './pages/Signin';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello</h1>
+    <Signin />
     <GlobalStyle />
   </>
 );
