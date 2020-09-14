aec881a1c8d1c96760374400ca83a80ae9ad6570
Nykolle Malone <malone.nykolle@gmail.com>
2020-09-14 20:17:09 -0300

    sign up page done

--- frontend-gobarber/src/App.tsx
+++ frontend-gobarber/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <Signin />
+    <Signup />
     <GlobalStyle />
   </>
 );
