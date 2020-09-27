dd86c4f6a50268270f35d3797b6ce21558284d1f
Nykolle Malone <malone.nykolle@gmail.com>
2020-09-27 16:39:39 -0300

    private routes added

--- frontend-gobarber/src/routes/index.tsx
+++ frontend-gobarber/src/routes/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
+
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Signin} />
     <Route path="/signup" component={Signup} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
