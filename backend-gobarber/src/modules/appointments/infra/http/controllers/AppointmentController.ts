4e11375e32871d4912b6dba2b37ff30affd388b0
Nykolle Malone <malone.nykolle@gmail.com>
2020-11-11 22:33:49 -0300

    jest config in progress

---
+++ backend-gobarber/src/modules/appointments/infra/http/controllers/AppointmentController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express';
+
+export default class AppointmentController {
+    public async create(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {}
+}
