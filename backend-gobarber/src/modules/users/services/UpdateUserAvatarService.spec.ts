743e2883ae8296eba2cfd5beb6439cec8f065b5f
Nykolle Malone <malone.nykolle@gmail.com>
2020-11-19 06:59:33 -0300

    update avatar tests created

---
+++ backend-gobarber/src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,79 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeStorageProvider from '@shared/container/providers/StorageProviders/fakes/FakeStorageProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+describe('UpdateUserAvatar', () => {
+    it('should be able to create a new user', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeStorageProvider = new FakeStorageProvider();
+
+        const updateUserAvatar = new UpdateUserAvatarService(
+            fakeUsersRepository,
+            fakeStorageProvider,
+        );
+
+        // como eu não tenho user id, antes eu preciso criar um usuário
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@gmail.com',
+            password: '123456',
+        });
+
+        await updateUserAvatar.execute({
+            user_id: user.id,
+            avatarFilename: 'avatar.jpg',
+        });
+
+        expect(user.avatar).toBe('avatar.jpg');
+    });
+
+    it('should not be able to update avatar from non existing user', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeStorageProvider = new FakeStorageProvider();
+
+        const updateUserAvatar = new UpdateUserAvatarService(
+            fakeUsersRepository,
+            fakeStorageProvider,
+        );
+
+        expect(
+            updateUserAvatar.execute({
+                user_id: 'non-existing-user',
+                avatarFilename: 'avatar.jpg',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should deleter old avatar when updating a new one', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeStorageProvider = new FakeStorageProvider();
+
+        const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile'); // me retorna a função que eu quero espionar, e com isso eu consigo saber se essa função foi disparada
+
+        const updateUserAvatar = new UpdateUserAvatarService(
+            fakeUsersRepository,
+            fakeStorageProvider,
+        );
+
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@gmail.com',
+            password: '123456',
+        });
+
+        await updateUserAvatar.execute({
+            user_id: user.id,
+            avatarFilename: 'avatar.jpg',
+        });
+
+        await updateUserAvatar.execute({
+            user_id: user.id,
+            avatarFilename: 'avatar2.jpg',
+        });
+
+        expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+        expect(user.avatar).toBe('avatar2.jpg');
+    });
+});
