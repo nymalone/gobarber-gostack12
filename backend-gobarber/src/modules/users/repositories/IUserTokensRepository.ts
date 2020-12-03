f48c9b0b8799acb67112b6a6143856b2c90636de
Nykolle Malone <malone.nykolle@gmail.com>
2020-12-03 06:41:11 -0300

    first test to reset password done

--- backend-gobarber/src/modules/users/repositories/IUserTokensRepository.ts
+++ backend-gobarber/src/modules/users/repositories/IUserTokensRepository.ts
@@ -4,4 +4,5 @@ import UserToken from '../infra/typeorm/entities/UserToken';
 export default interface IUserTokensRepository {
     // vai gerar um token para um usuário especifico
     generate(user_id: string): Promise<UserToken>;
+    findByToken(token: string): Promise<UserToken | undefined>;
 }
